0e6ea3355ef69387cf3301eb8cd7bdbe99549c44
Piettra <piettra_cidral@estudante.sesisenai.org.br>
2026-04-03 23:00:42 -0300

    ADD - Regas de Negocio adcionadas

--- script.js
+++ script.js
@@ -17,3 +17,53 @@ possuiQuintal.addEventListener("change", function() {
     quintalSeguroDiv.classList.add("hidden");
     if (this.value === "sim") quintalSeguroDiv.classList.remove("hidden");
 });
+
+document.getElementById("formulario").addEventListener("submit", function(e) {
+    let erros = [];
+
+    const idade = Number(document.getElementById("idade").value);
+    const tel = document.getElementById("telefone").value.replace(/\D/g,'');
+    const cpf = document.getElementById("cpf").value;
+    const moradiaVal = moradia.value;
+    const permite = document.getElementById("permiteAnimais").value;
+    const possuiQuintalVal = possuiQuintal.value;
+    const quintalSeguro = document.getElementById("quintalSeguro").value;
+    const pet = document.getElementById("pet").value;
+    const horas = Number(document.getElementById("hora").value);
+    const motivo = document.getElementById("mensagem").value.toLowerCase();
+    const financeiro = document.getElementById("financeiro").value;
+    const decisao = document.getElementById("decisao").value;
+    const termo = document.getElementById("termo").checked;
+
+    if (idade < 18) erros.push("Deve ter 18 anos ou mais");
+
+    if (tel.length < 8) erros.push("Telefone inválido (mínimo 8 números)");
+
+    if (cpfsCadastrados.includes(cpf)) erros.push("CPF já cadastrado");
+
+    if (moradiaVal === "apartamento" && permite !== "sim") erros.push("Apartamento deve permitir animais");
+
+    if (moradiaVal === "casa") {
+        if (possuiQuintalVal !== "sim") erros.push("Casa precisa ter quintal");
+        
+        if (possuiQuintalVal === "sim" && quintalSeguro !== "sim") erros.push("O quintal precisa ser seguro");
+    }
+
+    if (horas > 8) erros.push("O pet ficará mais de 8 horas sozinho! Justifique melhor");
+
+    if (pet === "nao") alert("A ONG poderá acompanhar sua adaptação com o pet");
+
+    const motivosInvalidos = ["quero", "porque sim"];
+    if (motivosInvalidos.some(m => motivo.includes(m))) erros.push("Motivo muito genérico");
+
+    if (financeiro === "ruim") erros.push("Condição financeira insuficiente");
+
+    if (decisao === "hoje") erros.push("Atenção: sua decisão pode estar sendo impulsiva");
+
+    if (!termo) erros.push("Você precisa aceitar os termos");
+
+    if (erros.length) {
+        e.preventDefault();
+        alert("Erros encontrados:\n" + erros.join("\n"));
+    }
+});
