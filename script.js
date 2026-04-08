39397458dd013c857b4ab45bdb9811d23c102c7a
piettra <piettra_cidral@estudante.sesisenai.org.br>
2026-04-08 11:27:51 -0300

    Add- Recuperei as estruturas que perdi na última aula

--- script.js
+++ script.js
@@ -1,69 +1,97 @@
-const cpfsCadastrados = ["11111111111"];
+function validacao () {
 
-const moradia = document.getElementById("moradia");
-const apartamentoDiv = document.getElementById("apartamentoDiv");
-const casaDiv = document.getElementById("casaDiv");
-const possuiQuintal = document.getElementById("possuiQuintal");
-const quintalSeguroDiv = document.getElementById("quintalSeguroDiv");
+    let valido = true;
+    let nome = document.getElementByld("nome").value;
+    let email = document.getElementByld("email").value;
+    let telefone = document.getElementByld("telefone").value;
+    let cidade = document.getElementByld("idade").value;
+    let horas = document.getElementByld("horas").value;
+    let motivo = document.getElementByld("motivo").value;
+    let cpf = document.getElementByld("cpf").value;
+    let moradia = document.querySelector('input [name="moradia"]:checked');
+    let quintal = document.querySelector('input [name="quintal"]:checked');
+    let pet_antes = document.querySelector('input [name="pet_antes"]:checked');
+    let finaceiro = document.querySelector('input [name="financeiro"]:checked');
+    let cpf_registrado = [
+        "11111111111",
+        "12345678909",
+        "12324354657",
+        "98765432112"
+    ]
+    
+    let termo = document.getElementById("termo")
 
-moradia.addEventListener("change", function() {
-    apartamentoDiv.classList.add("hidden");
-    casaDiv.classList.add("hidden");
-    if (this.value === "apartamento") apartamentoDiv.classList.remove("hidden");
-    if (this.value === "casa") casaDiv.classList.remove("hidden");
-});
-
-possuiQuintal.addEventListener("change", function() {
-    quintalSeguroDiv.classList.add("hidden");
-    if (this.value === "sim") quintalSeguroDiv.classList.remove("hidden");
-});
-
-document.getElementById("formulario").addEventListener("submit", function(e) {
-    let erros = [];
-
-    const idade = Number(document.getElementById("idade").value);
-    const tel = document.getElementById("telefone").value.replace(/\D/g,'');
-    const cpf = document.getElementById("cpf").value;
-    const moradiaVal = moradia.value;
-    const permite = document.getElementById("permiteAnimais").value;
-    const possuiQuintalVal = possuiQuintal.value;
-    const quintalSeguro = document.getElementById("quintalSeguro").value;
-    const pet = document.getElementById("pet").value;
-    const horas = Number(document.getElementById("hora").value);
-    const motivo = document.getElementById("mensagem").value.toLowerCase();
-    const financeiro = document.getElementById("financeiro").value;
-    const decisao = document.getElementById("decisao").value;
-    const termo = document.getElementById("termo").checked;
-
-    if (idade < 18) erros.push("Deve ter 18 anos ou mais");
-
-    if (tel.length < 8) erros.push("Telefone inválido (mínimo 8 números)");
-
-    if (cpfsCadastrados.includes(cpf)) erros.push("CPF já cadastrado");
-
-    if (moradiaVal === "apartamento" && permite !== "sim") erros.push("Apartamento deve permitir animais");
-
-    if (moradiaVal === "casa") {
-        if (possuiQuintalVal !== "sim") erros.push("Casa precisa ter quintal");
-        
-        if (possuiQuintalVal === "sim" && quintalSeguro !== "sim") erros.push("O quintal precisa ser seguro");
+    if (nome.length === 0) {
+    alert ("informe seu nome");
+    valido = false;
+    } else if (nome.lenght < 3) {
+    alert ("O nome deve ter no mínimo 3 caracteres.");
+    valido = false;
     }
 
-    if (horas > 8) erros.push("O pet ficará mais de 8 horas sozinho! Justifique melhor");
+     if (email.length === 0) {
+    alert ("informe seu email");
+    valido = false;
+    } else if (!email.includes("@")) {
+    alert ("O email deve ter @");
+    valido = false;
 
-    if (pet === "nao") alert("A ONG poderá acompanhar sua adaptação com o pet");
-
-    const motivosInvalidos = ["quero", "porque sim"];
-    if (motivosInvalidos.some(m => motivo.includes(m))) erros.push("Motivo muito genérico");
-
-    if (financeiro === "ruim") erros.push("Condição financeira insuficiente");
-
-    if (decisao === "hoje") erros.push("Atenção: sua decisão pode estar sendo impulsiva");
-
-    if (!termo) erros.push("Você precisa aceitar os termos");
-
-    if (erros.length) {
-        e.preventDefault();
-        alert("Erros encontrados:\n" + erros.join("\n"));
     }
-});
+
+     if (telefone.lenght === 0) {
+    alert ("informe seu telefone");
+    valido = false;
+    } else if (telefone.lenght < 8) {
+    alert ("O telefone deve ter no mínimo 8 dígitos.");
+    valido = false;
+    }
+
+     if (cpf.lenght == 0) {
+    alert ("Campo CPF obrigatório");
+    valido = false;
+    } else if (cpf_registrado.includes(cpf)) {
+    alert ("O cpf que você digitou já está cadastrado");
+    valido = false;
+    }
+
+     if (idade.lenght === 0) {
+    alert ("informe sua idade");
+    valido = false;
+    } else if (idade < 18) {
+    alert ("Você deve ser maior de idade para continuar o processo de adoção.");
+    valido = false;
+    }
+
+    if (!moradia){
+    alert("Selecione o tipo de moradia")
+    valido = false;
+    } else if (moradia && moradia.value === "apartamento" && quintal && quintal.value === "sim") {
+        alert ("Erro, quem mora em apartamento não pode ter quintal.");
+        valido = false;
+    } else if (moradia && moradia.value === "casa" && quintal && quintal.value === "nao") {
+        alert ("Aviso: por naõ possuir quintal, o uso de espaço externo pode ser limitado");
+    }
+
+    if (!quintal) {
+        alert ("selecione se tem quintal");
+        valido = false;
+    }
+
+    if (!pet_antes) {
+        alert ("Você já teve animais alguma vez antes?");
+        valido = false;
+    } else if (pet_antes && pet_antes.value === "nao") {
+        alert ("Como você nunca teve nenhum amiguinho pet antes, pode haver um acompanhamento da ONG!");
+    }
+
+
+    if (!financeiro) {
+        alert ("Você tem condição financeira para ter um amiguinho pet?");
+        valido = false;
+    } else if (finaceiro && financeiro.value === "nao") {
+        alert ("Você marcou que não tem condição para cuidar de um animalzinho, opte por um momento em que você tenha condições de dar uma vida boa para seu pet");
+        valido = false;
+    }
+
+
+    }
